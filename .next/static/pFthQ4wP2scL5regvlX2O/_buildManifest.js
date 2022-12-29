a96290954edcbb75f44b6c9c82542c44c54881e1
Stephen Puthenpurackal <datfoosteve@gmail.com>
2022-12-29 00:48:36 -0600

    here we go

---
+++ .next/static/pFthQ4wP2scL5regvlX2O/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d3b91c1caf3413f2.js"],"/_error":["static/chunks/pages/_error-349c0b3527345874.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,16 @@
 import dynamic from 'next/dynamic';
 import react from 'react';
 
+
 const PDFViewer = dynamic(() => import('../components/pdf-viewer'), {
     ssr: false,
 });
 
 export default function PDF() {
     return (
-        <div className="container" mx-auto >
-        <div className="container mx-auto" mx-auto >
-            
-            <div className="flex justify-center"flex justify-center>
-            <PDFViewer />
+            <div >
+            <PDFViewer/>
             </div>
-        </div>
-        </div>
+      
     );
-}
+} 
